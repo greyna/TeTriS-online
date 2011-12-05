88702aa8095ea1fab6232ff4149f8da5582a4ca0
Arthur Van-Ceulen <vandekeu@gmail.com>
2011-12-05 21:22:47 +0100

    v1.0

--- tetris.js
+++ tetris.js
@@ -37,18 +37,30 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 						return true; // alors la gravité créerait une collision
 		return false;
 	};
-	this.solve = function() { // pour modifier la matrice lorsqu'une ligne est complète
-		var complete;
+	
+	// pour modifier la matrice lorsqu'une ligne est complète (tombée des lignes au-dessus)
+	this.solve = function() { // renvoie false si game over
+		var complete, gameOver=0;
+		
 		for (var i=0; i < 22; i++) { // pour chaque ligne de haut en bas
 			complete = 1;
 			for (var j=0; (j<30)&&(complete); j++) { // pour chaque élément de chaque ligne
 				if (!him.matrice[i][j]) complete = 0; // s'il n'y a pas de cube, alors la ligne n'est pas complète
 			}
 			if (complete) {
+				score++;
+				articlep.innerHTML='SCORE : '+score;
 				for (var k=i; k > 0; k--) // pour chaque ligne depuis la ligne complète de bas en haut
 						him.matrice[k]=him.matrice[k-1]; // on copie la ligne d'au dessus en dessous
 			}
 		}
+		
+		//game over s'il existe des cubes dans l'emplacement d'arrivée d'une nouvelle pièce
+		for (var i=0; (i<4)&&(!gameOver); i++)
+			for (var j=11; (j<15)&&(!gameOver); j++)
+				if (him.matrice[i][j]) gameOver=1;
+		if (gameOver) return false;
+		else return true;
 	};
 	this.draw = function() {
 		ctx.clearRect(0,0,600,440);
@@ -92,22 +104,60 @@ function Piece(xp,yp,x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4,v1,v2,v3,v4) { // class
 	// Les variables changent d'état en fonction des évènements (voir document.EventListener)
 	if (moveR) {
 		moveR=0;
-		if (him.xp<560) him.xp+=20; // translation Right d'un cube (20px)
+		var ok=1;
+		switch (him.xp) { // en fonction de la position de la matrice en dehors du canvas, test si la piece serait affiché hors du canvas et dans ce cas pas de mouvement
+			case 520: 
+				for (var i=0; (i<4)&&ok; i++)
+					if (him.matrice[i][3]) ok=0;
+				break;
+			case 540: 
+				for (var i=0; (i<4)&&ok; i++)
+					if (him.matrice[i][2]) ok=0;
+				break;
+			case 560: 
+				for (var i=0; (i<4)&&ok; i++)
+					if (him.matrice[i][1]) ok=0;
+				break;
+			case 580: ok=0; break;
+			default: ok=1; break;
+		}
+		if (ok) him.xp+=20;	// translation Right d'un cube (+20px)
+		ok=1;
 	}
-	if (moveL) {
+	else if (moveL) { // si l'utilisateur va à gauche et à droite en même temps, ben il ira à droite et ça bugera pas :p
 		moveL=0;
-		if (him.xp>0) him.xp-=20; // translation Left d'un cube (-20px)
+		var ok=1;
+		switch (him.xp) { // en fonction de la position de la matrice en dehors du canvas, test si la piece serait affiché hors du canvas et dans ce cas pas de mouvement
+			case 0: 
+				for (var i=0; (i<4)&&ok; i++)
+					if (him.matrice[i][0]) ok=0;
+				break;
+			case -20: 
+				for (var i=0; (i<4)&&ok; i++)
+					if (him.matrice[i][1]) ok=0;
+				break;
+			case -40: 
+				for (var i=0; (i<4)&&ok; i++)
+					if (him.matrice[i][2]) ok=0;
+				break;
+			case -60: ok=0; break;
+			default: ok=1; break;
+		}
+		if (ok) him.xp-=20;	// translation Left d'un cube (-20px)
+		ok=1;
     }
 	if (turn) {
 		turn=0;
-		var tmp; // ci-dessous l'algorithme permettant de faire une rotation de centre (i=1,j=1)de la matrice 4x4 représentant la pièce
-		for (var i=0; i < 2; i++) {
-			for (var j=0; j < 2; j++) {					
-				tmp=him.matrice[i][j];
-				him.matrice[i][j]=him.matrice[j][3-i];
-				him.matrice[j][3-i]=him.matrice[3-i][3-j];
-				him.matrice[3-i][3-j]=him.matrice[3-j][i];
-				him.matrice[3-j][i]=tmp;
+		if ((him.xp>=0)&&(him.xp<=520)) { // l'utilisateur peut tourner uniquement si la matrice représentant la piece est dans le canvas
+			var tmp; // ci-dessous l'algorithme permettant de faire une rotation de centre (i=1,j=1)de la matrice 4x4 représentant la pièce
+			for (var i=0; i < 2; i++) {
+				for (var j=0; j < 2; j++) {					
+					tmp=him.matrice[i][j];
+					him.matrice[i][j]=him.matrice[j][3-i];
+					him.matrice[j][3-i]=him.matrice[3-i][3-j];
+					him.matrice[3-i][3-j]=him.matrice[3-j][i];
+					him.matrice[3-j][i]=tmp;
+				}
 			}
 		}
 	}
@@ -126,11 +176,13 @@ function Piece(xp,yp,x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4,v1,v2,v3,v4) { // class
 	else {
 		map.add(him);
 		//map.display();
-		map.solve();
-		map.draw();
-		him = new Piece(300,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
-		straightToBot = 0;
-		him.moveToBot(period);
+		if (map.solve()) {
+			map.draw();
+			him = new Piece(260,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+			straightToBot = 0;
+			him.moveToBot(period);
+		}
+		else alert('GAME OVER!');
 	}
   };
 }
@@ -139,7 +191,7 @@ function Piece(xp,yp,x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4,v1,v2,v3,v4) { // class
 // INIT
 var canvas = document.getElementById('tutorial');
 var articlep = document.querySelector('article p');
-var straightToBot = 0, bot = 0, moveL=0, moveR=0, turn=0;
+var straightToBot = 0, bot = 0, moveL=0, moveR=0, turn=0, score=0;
 if (canvas.getContext){
 	var ctx = canvas.getContext('2d');
 }
@@ -163,5 +215,5 @@ var t2 = new Piece(100,300,0,0,1,0,1,1,1,0,0,0,1,0,0,0,0,0);
 var cube2 = new Piece(0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0);
 var t = new Piece(100,0,0,0,1,0,1,1,1,0,0,0,1,0,0,0,0,0);
 //map.display();
-t.moveToBot(10);
+t.moveToBot(20);
 //map.draw();

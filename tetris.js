096db68b3c1e2b4c1bd6e8c2d5f94cdb741c7893
Arthur Van-Ceulen <vandekeu@gmail.com>
2011-12-07 00:45:51 +0100

    v1.2

--- tetris.js
+++ tetris.js
@@ -6,9 +6,9 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 	this.init = function() {
 		for (var i=0; i < 23; i++) { // 22+1 ligne pour le sol
 			him.matrice[i] = new Array();
-			for (var j=0; j<30; j++) { // 30 colonnes
-				if (i!==22) him.matrice[i][j]=0;
-				else him.matrice[i][j]=1; // sol
+			for (var j=0; j<32; j++) { // 30+2 colonnes pour les murs
+				if ((i!==22)&&(j!==0)&&(j!==31)) him.matrice[i][j]=0;
+				else him.matrice[i][j]=1; // sol ou mur
 			}
 		}
 	};
@@ -16,14 +16,14 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 		var x = (piece.xp/20)|0; // cast from double vers integer
 		var y = (piece.yp/20)|0; // ici on récupère la position dans la matrice en fonction de la position sur le canvas (en pixels)
 		for (var i=y; i < y+4; i++)
-			for (var j=x; j < x+4; j++)
-				if (piece.matrice[i-y][j-x]) him.matrice[i][j]=piece.matrice[i-y][j-x];
+			for (var j=x+1; j < x+5; j++)
+				if (piece.matrice[i-y][j-x-1]) him.matrice[i][j]=piece.matrice[i-y][j-x-1];
 	};
 	this.display = function() { // affiche la matrice avec les 0 et 1 (pour le développeur)
 		articlep.innerHTML='';
 		for (var i=0; i < 23; i++) {
 			articlep.innerHTML+='<br/>';
-			for (var j=0; j<30; j++)
+			for (var j=0; j<32; j++)
 				articlep.innerHTML+=him.matrice[i][j];
 		}
 	};
@@ -31,8 +31,8 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 		var x = (piece.xp/20)|0; // cast from double to integer
 		var y = (piece.yp/20)|0;
 		for (var i=y; i < y+4; i++)
-			for (var j=x; j < x+4; j++)
-				if (piece.matrice[i-y][j-x]) // s'il y a un cube de la piece ici,
+			for (var j=x+1; j < x+5; j++)
+				if (piece.matrice[i-y][j-x-1]) // s'il y a un cube de la piece ici,
 					if (him.matrice[i+1][j]) // s'il y a un cube de la map en-dessous,
 						return true; // alors la gravité créerait une collision
 		return false;
@@ -41,8 +41,8 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 		var x = (piece.xp/20)|0; // cast from double to integer
 		var y = (piece.yp/20)|0;
 		for (var i=y; i < y+4; i++)
-			for (var j=x; j < x+4; j++)
-				if (piece.matrice[i-y][j-x]) { // s'il y a un cube de la piece ici,
+			for (var j=x+1; j < x+5; j++)
+				if (piece.matrice[i-y][j-x-1]) { // s'il y a un cube de la piece ici,
 					if (him.matrice[i][j-1]) // et s'il y a un cube de la map à gauche
 						return -1;
 					if (him.matrice[i][j+1]) // et s'il y a un cube de la map à droite
@@ -56,7 +56,7 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 		
 		for (var i=0; i < 22; i++) { // pour chaque ligne de haut en bas
 			complete = 1;
-			for (var j=0; (j<30)&&(complete); j++) { // pour chaque élément de chaque ligne
+			for (var j=1; (j<31)&&(complete); j++) { // pour chaque élément de chaque ligne
 				if (!him.matrice[i][j]) complete = 0; // s'il n'y a pas de cube, alors la ligne n'est pas complète
 			}
 			if (complete) {
@@ -69,7 +69,7 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 		
 		//game over s'il existe des cubes dans l'emplacement d'arrivée d'une nouvelle pièce
 		for (var i=0; (i<4)&&(!gameOver); i++)
-			for (var j=11; (j<15)&&(!gameOver); j++)
+			for (var j=12; (j<16)&&(!gameOver); j++)
 				if (him.matrice[i][j]) gameOver=1;
 		if (gameOver) return false;
 		else return true;
@@ -78,8 +78,8 @@ function Map() { // Map des pièces déposées sous forme matricielle (1 si cube
 	this.draw = function() {
 		ctx.clearRect(0,0,600,440);
 		for (var i=0; i < 22; i++)
-			for (var j=0; j < 30; j++)
-				if (him.matrice[i][j]) ctx.fillRect(j*20,i*20,20,20);
+			for (var j=1; j < 31; j++)
+				if (him.matrice[i][j]) ctx.fillRect((j-1)*20,i*20,20,20);
 	};
 }
 
@@ -161,74 +161,43 @@ function Piece() { // class Piece
   pour enfin réappeller la méthode sur le même objet afin qu'une nouvelle piece apparaisse au joueur et descende à son tour */
   this.moveToBot = function (period) {
 	him.clear(); // on efface le dessin de la pièce actuel avant modification
-	//affichage coordonnées pièce, décommentez ci-dessous
+	
+	//affichage coordonnées pièce en direct (waow lé fort le tutur), décommentez ci-dessous
 	//articlep.innerHTML='x: '+him.xp+'<br/>y: '+him.yp;
 	
     him.yp+=1; // descente d'un pixel
 	// Les variables changent d'état en fonction des évènements (voir document.EventListener)
 	if (moveR) {
 		moveR=0;
-		if (map.collisionHorizontale(him)!==1) {
-			var ok=1;
-			switch (him.xp) { // en fonction de la position de la matrice en dehors du canvas, test si la piece serait affiché hors du canvas et dans ce cas pas de mouvement
-				case 520: 
-					for (var i=0; (i<4)&&ok; i++)
-						if (him.matrice[i][3]) ok=0;
-					break;
-				case 540: 
-					for (var i=0; (i<4)&&ok; i++)
-						if (him.matrice[i][2]) ok=0;
-					break;
-				case 560: 
-					for (var i=0; (i<4)&&ok; i++)
-						if (him.matrice[i][1]) ok=0;
-					break;
-				case 580: ok=0; break;
-				default: ok=1; break;
-			}
-			if (ok) him.xp+=20;	// translation Right d'un cube (+20px)
-			ok=1;
-		}
+		if (map.collisionHorizontale(him)!==1) him.xp+=20;	// translation Right d'un cube (+20px)
 	}
 	else if (moveL) { // si l'utilisateur va à gauche et à droite en même temps, ben il ira à droite et ça bugera pas :p
 		moveL=0;
-		if (map.collisionHorizontale(him)!==-1) {
-			var ok=1;
-			switch (him.xp) { // en fonction de la position de la matrice en dehors du canvas, test si la piece serait affiché hors du canvas et dans ce cas pas de mouvement
-				case 0: 
-					for (var i=0; (i<4)&&ok; i++)
-						if (him.matrice[i][0]) ok=0;
-					break;
-				case -20: 
-					for (var i=0; (i<4)&&ok; i++)
-						if (him.matrice[i][1]) ok=0;
-					break;
-				case -40: 
-					for (var i=0; (i<4)&&ok; i++)
-						if (him.matrice[i][2]) ok=0;
-					break;
-				case -60: ok=0; break;
-				default: ok=1; break;
-			}
-			if (ok) him.xp-=20;	// translation Left d'un cube (-20px)
-			ok=1;
-		}
+		if (map.collisionHorizontale(him)!==-1) him.xp-=20;	// translation Left d'un cube (-20px)
 	}
 	if (turn) {
 		turn=0;
-		if ((him.xp>=0)&&(him.xp<=520)) { // l'utilisateur peut tourner uniquement si la matrice représentant la piece est dans le canvas
-			var tmp; // ci-dessous l'algorithme permettant de faire une rotation de centre (i=1,j=1)de la matrice 4x4 représentant la pièce
-			for (var i=0; i < 2; i++) {
-				for (var j=0; j < 2; j++) {					
-					tmp=him.matrice[i][j];
-					him.matrice[i][j]=him.matrice[j][3-i];
-					him.matrice[j][3-i]=him.matrice[3-i][3-j];
-					him.matrice[3-i][3-j]=him.matrice[3-j][i];
-					him.matrice[3-j][i]=tmp;
-				}
+		var tmp; // ci-dessous l'algorithme permettant de faire une rotation de centre (i=1,j=1)de la matrice 4x4 représentant la pièce
+		for (var i=0; i < 2; i++)
+			for (var j=0; j < 2; j++) {					
+				tmp=him.matrice[i][j];
+				him.matrice[i][j]=him.matrice[j][3-i];
+				him.matrice[j][3-i]=him.matrice[3-i][3-j];
+				him.matrice[3-i][3-j]=him.matrice[3-j][i];
+				him.matrice[3-j][i]=tmp;
 			}
+		if ((map.collisionHorizontale(him)!==0)) { // dans le cas où tourner impliquerait une collision
+			for (var i=0; i < 2; i++) // on applique l'algorithme inverse
+				for (var j=0; j < 2; j++) {					
+					tmp=him.matrice[3-j][i];
+					him.matrice[3-j][i]=him.matrice[3-i][3-j];
+					him.matrice[3-i][3-j]=him.matrice[j][3-i];
+					him.matrice[j][3-i]=him.matrice[i][j];
+					him.matrice[i][j]=tmp;
+				}
 		}
 	}
+	
 	
     him.draw(); // on dessine la pièce modifiée
 	
@@ -257,11 +226,14 @@ function Piece() { // class Piece
 
 
 // INIT
-var canvas = document.getElementById('tutorial');
+var canvasJeu = document.getElementById('tetris');
+var canvasInfo = document.getElementById('info');
 var articlep = document.querySelector('article p');
 var straightToBot = 0, bot = 0, moveL=0, moveR=0, turn=0, score=0;
-if (canvas.getContext){
-	var ctx = canvas.getContext('2d');
+if (canvasJeu.getContext){
+	var ctx = canvasJeu.getContext('2d');
+}if (canvasInfo.getContext){
+	var ctxInfo = canvasInfo.getContext('2d');
 }
 ctx.fillStyle = "rgb(200,0,0)";
 document.addEventListener('keydown', function(e){
